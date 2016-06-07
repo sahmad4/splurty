3853b0867ed16ee924825d4c745b09fff84c0428
Salman Ahmad <salman_sv@yahoo.com>
2016-06-07 18:30:11 +0000

    Adjust fonts

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module Splurty
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+  #add the fonts path
+  config.assets.paths<< "#{rails.root}/app/assets/fonts"
+  #precompile additional assets
+  config.assets.precompile += %w(.svg .eot .woff .ttf .woff2)
   end
 end
